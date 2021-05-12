6ebf4de5e15fabfe0dcc09d5a31884c88e2ab57e
Andreev Anton <Anton.Andreev@indusoft.ru>
2021-05-12 15:58:52 +0300

    Открытие TL

--- main_page.py
+++ main_page.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 class MainPage(BasePage):
-    def in_to_login(self):
+    def in_to_login(self): #Авторизация
         username_fild = self.browser.find_element(By.ID, 'txtUserName')
         username_fild.clear()
         username_fild.send_keys("sam")
@@ -13,5 +13,20 @@ class MainPage(BasePage):
         enter_button = self.browser.find_element(By.CLASS_NAME, 'btn-login')
         enter_button.click()
 
-    def should_be_start_page(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Не авторизовался"    
+    def should_be_start_page(self): #Проверка аторизации
+        assert self.is_element_present(By.CSS_SELECTOR, ".custom-menu-single-elem-button"), "Не авторизовался"    
+
+    def open_tl(self): #Открытие TL
+        username_fild = self.browser.find_element(By.ID, 'txtUserName')
+        username_fild.clear()
+        username_fild.send_keys("sam")
+        password_fild = self.browser.find_element(By.ID, 'txtPassword')
+        password_fild.clear()
+        password_fild.send_keys("sam")
+        enter_button = self.browser.find_element(By.CLASS_NAME, 'btn-login')
+        enter_button.click()
+        tl_button = self.browser.find_element(By.CSS_SELECTOR, '.custom-menu-single-elem-button')
+        tl_button.click()
+
+    def check_open_tl(self):
+        assert self.is_element_present(By.CSS_SELECTOR, 'role="grid"'), "TL не открылся"
--- test_tl.py
+++ test_tl.py
@@ -4,5 +4,12 @@ def test_go_to_login_page(browser):
     link = "http://192.168.36.28:8093"
     page = MainPage(browser, link)
     page.open()
-    page.in_to_login()
-    page.should_be_start_page()
+    page.in_to_login()             #Авторизация на портале
+    page.should_be_start_page()    #Проверка автризации
+
+def test_go_to_tl(browser):
+    link = "http://192.168.36.28:8093"
+    page = MainPage(browser, link)
+    page.open()
+    page.open_tl()
+    page.check_open_tl() 
