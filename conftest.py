59dacb6b509747225360c1a5656a8aef08f96464
Andreev Anton <Anton.Andreev@indusoft.ru>
2021-05-12 17:11:07 +0300

    Добавил conftest

--- conftest.py
+++ conftest.py
@@ -1,8 +1,9 @@
 import pytest
 from selenium import webdriver
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 def browser():    
     browser = webdriver.Chrome()
+    browser.maximize_window()
     yield browser    
     browser.quit()
